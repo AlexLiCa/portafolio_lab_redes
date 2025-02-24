76044160090557fe51d77e4fc57f7778eaa0625d
AlexLiCa <alexlin.carre@gmail.com>
2025-02-23 19:43:03 -0600

    -Generacion dinamoca de las practicas y de los autores ya casi funcional

---
+++ autores.js
@@ -0,0 +1,20 @@
+const autoresGlobales = [
+  {
+    name: "Alejandro Linares Carreón",
+    image: "assets/Alejandro.jpg",
+    email: "alejandro.linares.carreon@iberopuebla.mx",
+    carrera: "Ingenieria en sistemas computacionales",
+  },
+  {
+    name: "Erick",
+    image: "assets/Erick.jpg",
+    email: "erick.fischer@iberopuebla.mx",
+    carrera: "Ingenieria en mecatronica",
+  },
+  {
+    name: "Lalo",
+    image: "assets/Lalo.jpg",
+    email: "Lalo@ibero.mx",
+    carrera: "Ingenieria en mecatronica",
+  },
+];
